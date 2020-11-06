9f8003a6b9f739a78e0ff180cab014b6116652e4
Elliott Lockwood <lockwood721@gmail.com>
2020-11-06 12:10:49 -0600

    jumbotron addition

--- tw/src/App.js
+++ tw/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Again
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+
+    <Jumbotron>
+  <h1>Hello, world!</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+  <p>
+    <Button variant="primary">Learn more</Button>
+  </p>
+</Jumbotron>
+
+  )
+};
 
 export default App; 
