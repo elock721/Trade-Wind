492c38053e775b1b89bb0433d8c891ce33c59178
Elliott Lockwood <lockwood721@gmail.com>
2020-11-13 23:55:31 -0600

    accordian

--- tw/src/App.js
+++ tw/src/App.js
@@ -2,7 +2,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 // import Jumbotron from 'react-bootstrap/Jumbotron';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card'
+import Accordion from 'react-bootstrap/Accordion'
 import Card from 'react-bootstrap/Card'
 import './App.css';
 
@@ -20,9 +22,30 @@ function App() {
 //   </p>
 // </Jumbotron>
 
+<Accordion defaultActiveKey="0">
+  <Card>
+    <Card.Header>
+      <Accordion.Toggle as={Button} variant="link" eventKey="0">
+        Click me!
+      </Accordion.Toggle>
+    </Card.Header>
+    <Accordion.Collapse eventKey="0">
+      <Card.Body>Hello! I'm the body</Card.Body>
+    </Accordion.Collapse>
+  </Card>
+  <Card>
+    <Card.Header>
+      <Accordion.Toggle as={Button} variant="link" eventKey="1">
+        Click me!
+      </Accordion.Toggle>
+    </Card.Header>
+    <Accordion.Collapse eventKey="1">
+      <Card.Body>Hello! I'm another body</Card.Body>
+    </Accordion.Collapse>
+  </Card>
+</Accordion>
 
-
-<Card style={{ width: '18rem' }}>
+{/* <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180" />
   <Card.Body>
     <Card.Title>Card Title</Card.Title>
@@ -32,7 +55,7 @@ function App() {
     </Card.Text>
     <Button variant="primary">Go somewhere</Button>
   </Card.Body>
-</Card>
+</Card> */}
 
 //testing 
 
